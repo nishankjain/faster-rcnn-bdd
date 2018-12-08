404c28e6a69064cc6eb67deb4a06016dea9a4c2d
nishankjain <nishankiit@gmail.com>
2018-12-08 10:06:31 +0000

    Working initial demo with bdd images

--- lib/make.sh
+++ lib/make.sh
@@ -2,22 +2,24 @@
 
 # CUDA_PATH=/usr/local/cuda/
 
-export CUDA_PATH=/usr/local/cuda/
+export CUDA_PATH=/usr/local/cuda-9.0/
 #You may also want to ad the following
 #export C_INCLUDE_PATH=/opt/cuda/include
 
 export CXXFLAGS="-std=c++11"
 export CFLAGS="-std=c99"
 
-python setup.py build_ext --inplace
+python3 setup.py build_ext --inplace
 rm -rf build
 
-CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
-           -gencode arch=compute_35,code=sm_35 \
-           -gencode arch=compute_50,code=sm_50 \
-           -gencode arch=compute_52,code=sm_52 \
-           -gencode arch=compute_60,code=sm_60 \
-           -gencode arch=compute_61,code=sm_61 "
+#CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
+ #          -gencode arch=compute_35,code=sm_35 \
+  #         -gencode arch=compute_50,code=sm_50 \
+   #        -gencode arch=compute_52,code=sm_52 \
+    #       -gencode arch=compute_60,code=sm_60 \
+     #      -gencode arch=compute_61,code=sm_61 "
+
+CUDA_ARCH="-gencode arch=compute_61,code=sm_61"
 
 # compile NMS
 cd model/nms/src
@@ -26,7 +28,7 @@ nvcc -c -o nms_cuda_kernel.cu.o nms_cuda_kernel.cu \
 	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
 
 cd ../
-python build.py
+python3 build.py
 
 # compile roi_pooling
 cd ../../
@@ -35,7 +37,7 @@ echo "Compiling roi pooling kernels by nvcc..."
 nvcc -c -o roi_pooling.cu.o roi_pooling_kernel.cu \
 	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
 cd ../
-python build.py
+python3 build.py
 
 # compile roi_align
 cd ../../
@@ -44,7 +46,7 @@ echo "Compiling roi align kernels by nvcc..."
 nvcc -c -o roi_align_kernel.cu.o roi_align_kernel.cu \
 	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
 cd ../
-python build.py
+python3 build.py
 
 # compile roi_crop
 cd ../../
@@ -53,4 +55,4 @@ echo "Compiling roi crop kernels by nvcc..."
 nvcc -c -o roi_crop_cuda_kernel.cu.o roi_crop_cuda_kernel.cu \
 	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
 cd ../
-python build.py
+python3 build.py
