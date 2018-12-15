2b184a3e3242d631f1fd82bef5fc1254852df99c
nishankjain <nishankjain@users.noreply.github.com>
2018-12-15 03:19:33 -0500

    Final fix2?

--- trainval_net.py
+++ trainval_net.py
@@ -288,8 +288,8 @@ if __name__ == '__main__':
     optimizer.load_state_dict(checkpoint['optimizer'])
     for state in optimizer.state.values():
       for k, v in state.items():
-          if isinstance(v, torch.Tensor):
-              state[k] = v.cuda()
+          if torch.is_tensor(v):
+            state[k] = v.cuda()
     lr = optimizer.param_groups[0]['lr']
     if 'pooling_mode' in checkpoint.keys():
       cfg.POOLING_MODE = checkpoint['pooling_mode']
